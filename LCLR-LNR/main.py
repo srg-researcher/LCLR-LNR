a3520c02e8994db33e29c35e98f59d1df90df4d9
srg-researcher <pangying1104@outlook.com>
2025-02-24 21:48:32 +0800

    Update main.py

--- LCLR-LNR/main.py
+++ LCLR-LNR/main.py
@@ -28,8 +28,6 @@ import torchvision
 import sys
 import time
 
-# Ying: delete redundant codes
-# for symmetric first window size equals to 5
 
 ## For plotting the logs
 # import wandb
@@ -276,7 +274,7 @@ def linear_rampup(current, warm_up, rampup_length=16):
 
 class SemiLoss(object):
     def __call__(self, outputs_x, targets_x, outputs_u, targets_u, epoch, warm_up):
-        probs_u = torch.softmax(outputs_u, dim=1)  # 感觉一个用的是交叉熵，一个用的是均方误差
+        probs_u = torch.softmax(outputs_u, dim=1) 
         Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
         Lu = torch.mean((probs_u - targets_u) ** 2)
         return Lx, Lu, linear_rampup(epoch, warm_up)
